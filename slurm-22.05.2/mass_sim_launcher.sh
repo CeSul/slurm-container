cf63d7616300e3728381f64eebe3fb8d4a35df09
Cesar Sul <csul@usc.edu>
2025-04-21 14:56:30 -0700

    start up workflow changes

--- slurm-22.05.2/mass_sim_launcher.sh
+++ slurm-22.05.2/mass_sim_launcher.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
-for i in `seq 0 81`
+container_name_list=()
+echo "Starting up containers..."
+for i in `seq 0 80`
 #for i in `seq 2 80`
 do
 
 container_name="carc/slurm_vc:slurm-22.05.2_winter_conf_changes"
 container_id=$(docker run --detach --rm -it -e SLURM_NUMNODES=78 $container_name)
+container_name_list+="$container_id "
+echo $container_id
+
+done
+
+echo "Give them a moment to start up slurm..."
+sleep 90
+echo ""
+
+i=0
+for container_id in $container_name_list
+do
 outlog=job_log_${i}_${container_id}
 echo "Look for logs in $outlog"
 
-sleep 20
-echo "Container started"
 docker container exec $container_id /opt/slurm/bin/sinfo
 
 echo "updating slurm.conf"
@@ -23,11 +35,13 @@ docker container exec  $container_id /opt/slurm/bin/scontrol reconfigure
 sleep 10
 
 echo "Launching jobs"
+echo ""
 docker container exec -w"/home/spack" \
 --env LD_PRELOAD=/usr/lib64/libfaketime/libfaketime.so.1 \
 --env FAKETIME_TIMESTAMP_FILE=/etc/faketimerc \
 $container_id python3 create_slurm_jobs.py &>> $outlog &
 
+i=$((i+1))
 #time=$(date "+%Y-%m-%d %T"); echo "@$time x100" > faketimerc
 #sleep 20
 #kecho "Time to liven things up"
