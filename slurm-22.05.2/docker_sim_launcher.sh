102fa35fe6bda4fb0da91510bf98b8d618ee63d8
Cesar Sul <csul@usc.edu>
2025-05-09 09:55:57 -0700

    arbitrary start points in parameter space

--- slurm-22.05.2/docker_sim_launcher.sh
+++ slurm-22.05.2/docker_sim_launcher.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
-job_data='winter2025_epyc64_10days'
+#job_data='winter2025_epyc64_10days'
 
 container_id=$(docker run --detach --rm -it -e SLURM_NUMNODES=171 carc/slurm_vc:slurm-22.05.2_winter)
 
+sleep 60
+
+echo "Container started"
+docker container exec $container_id /opt/slurm-22.05.2/bin/sinfo
+
 #docker container cp $job_data ${container_id}:/home/spack/slurm_anon_epyc64_10days
 
-docker container exec -w"/home/spack" $container_id python3 create_slurm_jobs.py 
+sleep 60
+
+echo "Launching jobs"
+docker container exec -w"/home/spack" \
+--env LD_PRELOAD=/usr/lib64/libfaketime/libfaketime.so.1 \
+--env FAKETIME_TIMESTAMP_FILE=/etc/faketimerc \
+$container_id python3 create_slurm_jobs.py > joblog_${container_id} &
+
+#time=$(date "+%Y-%m-%d %T"); echo "@$time x100" > faketimerc
+#sleep 20
+#kecho "Time to liven things up"
+#docker container cp faketimerc ${container_id}:/etc/faketimerc
--- slurm-22.05.2/mass_sim_launcher.sh
+++ slurm-22.05.2/mass_sim_launcher.sh
@@ -2,9 +2,11 @@
 
 container_name_list=()
 echo "Starting up containers..."
-for i in `seq 0 80`
-#for i in `seq 2 80`
+first_container_id=0
+last_container_id=81
+for i in `seq $fist_container_id $last_container_id`
 do
+
 
 container_name="carc/slurm_vc:slurm-22.05.2_winter_conf_changes"
 container_id=$(docker run --detach --rm -it -e SLURM_NUMNODES=78 $container_name)
@@ -14,10 +16,11 @@ echo $container_id
 done
 
 echo "Give them a moment to start up slurm..."
-sleep 90
+#sleep 90
+sleep 1
 echo ""
 
-i=0
+i=$first_container_id
 for container_id in $container_name_list
 do
 outlog=job_log_${i}_${container_id}
